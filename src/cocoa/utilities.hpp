96059fa0afd93afe7b82ab793e05f2c50642a85c
Jason Pena <jasonpena@awkless.com>
2025-06-23 19:18:21 -0600

    feat(cocoa): add `cocoa::clear_bit`
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

--- src/cocoa/utilities.hpp
+++ src/cocoa/utilities.hpp
@@ -22,9 +22,24 @@ constexpr void set_bit(T& var)
     static_assert(Position < std::numeric_limits<T>::digits, "position exceeds maximum bit range");
     static_assert(std::is_integral<T>::value == true, "type is not an integral type");
     static_assert(std::is_unsigned<T>::value == true, "type is not an unsigned type");
-    var |= (T(1) << Position);
+    var |= static_cast<T>(T(1) << Position);
 }
 
+/// @brief Clear target bit in variable.
+///
+/// @pre Given variable must be an unsigned integral type.
+/// @pre Position must range between [0, n), where _n_ is the total number of bits
+///      available in target variable.
+///
+/// @param [in,out] var Target variable to clear bit inside of.
+template<typename T, size_t Position>
+constexpr void clear_bit(T& var)
+{
+    static_assert(Position < std::numeric_limits<T>::digits, "position exceeds maximum bit range");
+    static_assert(std::is_integral<T>::value == true, "type is not an integral type");
+    static_assert(std::is_unsigned<T>::value == true, "type is not an unsigned type");
+    var &= static_cast<T>(~(T(1) << Position)); 
+}
 } // namespace cocoa
 
 #endif // COCOA_UTILITIES_HPP
--- tests/unit_utilities.cpp
+++ tests/unit_utilities.cpp
@@ -7,11 +7,20 @@
 
 #include <catch2/catch_test_macros.hpp>
 
-TEST_CASE("void set_bit<T, Position>(T& data)", "[set_bit]")
+TEST_CASE("constexpr void cocoa::set_bit(T&)", "[set_bit]")
 {
     uint8_t data = 0x00;
     cocoa::set_bit<uint8_t, 0>(data);
     cocoa::set_bit<uint8_t, 4>(data);
     cocoa::set_bit<uint8_t, 7>(data);
     REQUIRE(data == 0b10010001);
+}
+
+TEST_CASE("constexpr void cocoa::clear_bit(T&)", "[clear_bit]")
+{
+    uint8_t data = 0xFF;
+    cocoa::clear_bit<uint8_t, 0>(data);
+    cocoa::clear_bit<uint8_t, 4>(data);
+    cocoa::clear_bit<uint8_t, 7>(data);
+    REQUIRE(data == 0b01101110);
 }
