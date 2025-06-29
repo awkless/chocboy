5fbe6c8f87398648a10284dca1d608ced804de99
Jason Pena <jasonpena@awkless.com>
2025-06-29 13:20:35 -0600

    feat(cocoa::gb): add `MemoryBus` and `MemoryMap`
    Setup the `gb` namespace through this commit. This namespace will
    contain the entire GameBoy implementation. Right now, the
    `memory.{hpp,cpp}` files will contain anything regarding memory
    management for peripherals of the GameBoy SoC.
    
    For now I provided a basic skeleton for the GameBoy memory bus through
    the `MemoryBus` type. The `MemoryMap` enum contains the ranges for the
    GameBoy memory map which will be useful in the future.
    
    Signed-off-by: Jason Pena <jasonpena@awkless.com>

---
+++ src/cocoa/gb/memory.hpp
@@ -0,0 +1,65 @@
+// SPDX-FileCopyrightText: 2025 Jason Pena <jasonpena@awkless.com>
+// SPDX-License-Identifier: MIT
+
+#ifndef COCOA_GB_MEMORY_HPP
+#define COCOA_GB_MEMORY_HPP
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <limits>
+#include <type_traits>
+
+namespace cocoa::gb {
+/// @brief GameBoy memory map ranges.
+///
+/// @see https://gbdev.io/pandocs/Memory_Map.html
+enum class MemoryMap : uint16_t {
+    Rom0Start = 0x0000,
+    Rom0End = 0x3FFF,
+    RomXStart = 0x4000,
+    RomXEnd = 0x7FFF,
+    VramStart = 0x8000,
+    VramEnd = 0x9FFF,
+    SramStart = 0xA000,
+    SramEnd = 0xBFFF,
+    Wram0Start = 0xC000,
+    Wram0End = 0xCFFF,
+    WramXStart = 0xD000,
+    WramXEnd = 0xDFFF,
+    EchoRamStart = 0xE000,
+    EchoRamEnd = 0xFDFF,
+    OamStart = 0xFE00,
+    OamEnd = 0xFE9F,
+    UnusableAreaStart = 0xFEA0,
+    UnusableAreaEnd = 0xFEFF,
+    IoStart = 0xFF00,
+    IoEnd = 0xFF7F,
+    HramStart = 0xFF80,
+    HramEnd = 0xFFFF,
+};
+
+constexpr size_t MEMORY_BUS_SIZE
+    = std::numeric_limits<std::underlying_type<MemoryMap>::type>::max() + 1;
+
+/// @brief GameBoy memory bus.
+///
+/// The GameBoy uses a 16-bit address bus with an 8-bit data bus, resulting in a 64 KiB memory bus.
+/// The hardware employs memory mapping instead of port mapping to communicate with peripherals on
+/// the SoC. Thus, this representation of the GameBoy memory bus is designed to be shared between
+/// implementations of the GameBoy hardware for data transmission and communication with each other
+/// much like how the original hardware does.
+///
+/// @see https://gbdev.io/pandocs/Memory_Map.html
+class MemoryBus final {
+public:
+    MemoryBus() = default;
+
+    ~MemoryBus() noexcept = default;
+
+private:
+    std::array<uint8_t, MEMORY_BUS_SIZE> m_bus;
+};
+} // namespace cocoa::gb
+
+#endif // COOCA_GB_MEMORY_HPP
